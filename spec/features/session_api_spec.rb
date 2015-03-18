33182972151152346c11ec6724c9acc073c71012
Taylor Hobbs <taylor@feemon.com>
2015-03-17 22:35:52 -0500

    Test: Session tests

---
+++ spec/features/session_api_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Session Api', :type => :request do
+  it 'authenticates with username' do
+    user = User.create!(user_attributes)
+    post '/session', { login: user.username, password: 'Testing1' }
+    returned = eval(response.body)
+    expect(returned[:api_key][:access_token]).to match(/\S{32}/)
+    expect(returned[:api_key][:user_id]).to eq user.id
+  end
+
+  it 'authenticates with email' do
+    user = User.create!(user_attributes)
+    post '/session', { login: user.email, password: 'Testing1' }
+    returned = eval(response.body)
+    expect(returned[:api_key][:access_token]).to match(/\S{32}/)
+    expect(returned[:api_key][:user_id]).to eq user.id
+  end
+
+  it 'does not authenticate with invalid info' do
+    user = User.create!(user_attributes)
+    post '/session', { login: user.email, password: 'no good' }
+    expect(response.status).to eq 401
+  end
+end
