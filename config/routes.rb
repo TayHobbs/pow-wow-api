f357090283a748f67e3ca0db720d454069f406cf
Taylor Hobbs <taylor@feemon.com>
2015-04-15 20:57:21 -0500

    Feature: Send a user a reset password email

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, except: [:new, :edit]
+  post 'user/:id/forgot' => 'users#forgotten_password'
   post 'session' => 'session#create'
 end
---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe UserMailer, type: :mailer do
+  let(:user) { User.create!(user_attributes) }
+  let(:mail) { UserMailer.send_forgotten_password_email(user) }
+
+  it 'renders the subject' do
+      expect(mail.subject).to eql('Pow Wow: Someone requested a password reset on your account')
+  end
+
+  it 'sends to the users email' do
+    expect(mail.to).to eql([user.email])
+  end
+
+  it 'sends from the correct email' do
+    expect(mail.from).to eql(['noreply@powwow.com'])
+  end
+
+  it 'includes the users username' do
+    expect(mail.body.encoded).to match(user.username)
+  end
+
+  it 'includes the reset-password email' do
+    expect(mail.body.encoded).to match("https://pow-wow-api.com/user/#{user.email}/reset-password")
+  end
+
+end
