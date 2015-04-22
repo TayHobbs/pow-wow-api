2db9738246a7fcd3bb2939bc08c388cfd4b5c82e
Taylor Hobbs <taylor@feemon.com>
2015-04-21 21:04:40 -0500

    Style: linting

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ActiveRecord::Base
   has_many :api_keys, :dependent => :delete_all
 
   validates :username, presence: true,
-                  uniqueness: { case_sensitive: false }
+                    uniqueness: { case_sensitive: false }
+
   validates :email, presence: true,
-                  format: /.+@.+\..+/i,
-                  uniqueness: { case_sensitive: false }
+                    format: /.+@.+\..+/i,
+                    uniqueness: { case_sensitive: false }
 
 
   def session_api_key
