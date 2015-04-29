28c655250db775f5b1dfe750d62b790aeaab6763
Taylor Hobbs <taylor@feemon.com>
2015-04-28 20:00:51 -0500

    Refactor: Change tests to be forgotten password email specific

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,27 +1,27 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  let(:user) { User.create!(user_attributes) }
-  let(:mail) { UserMailer.send_forgotten_password_email(user) }
+  let(:user)           { User.create!(user_attributes) }
+  let(:forgotten_mail) { UserMailer.send_forgotten_password_email(user) }
 
-  it 'renders the subject' do
-      expect(mail.subject).to eql('Pow Wow: Someone requested a password reset on your account')
+  it 'renders the subject in forgotten email' do
+      expect(forgotten_mail.subject).to eql('Pow Wow: Someone requested a password reset on your account')
   end
 
-  it 'sends to the users email' do
-    expect(mail.to).to eql([user.email])
+  it 'sends to the users email in forgotten email' do
+    expect(forgotten_mail.to).to eql([user.email])
   end
 
-  it 'sends from the correct email' do
-    expect(mail.from).to eql(['noreply@powwow.com'])
+  it 'sends from the correct email in forgotten email' do
+    expect(forgotten_mail.from).to eql(['noreply@powwow.com'])
   end
 
-  it 'includes the users username' do
-    expect(mail.body.encoded).to match(user.username)
+  it 'includes the users username in forgotten email' do
+    expect(forgotten_mail.body.encoded).to match(user.username)
   end
 
-  it 'includes the reset-password email' do
-    expect(mail.body.encoded).to match(
+  it 'includes the reset-password email in forgotten email' do
+    expect(forgotten_mail.body.encoded).to match(
       "https://pow-wow-api.com/user/#{user.email}/reset-password/#{user.password_reset_code}")
   end
 
