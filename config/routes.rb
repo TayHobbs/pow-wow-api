08d355d6c9ebf22e1912740d1d23bda54cd87d52
Taylor Hobbs <taylor@feemon.com>
2015-06-12 08:15:35 -0500

    Feature: Project controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, except: [:new, :edit]
+  resources :project
   get 'users/:email/forgot' => 'users#forgotten_password', :constraints => { :email => /.+@.+\..*/ }
   get 'users/:email/reset/:reset_code' => 'users#reset_password', :constraints => { :email => /.+@.+\..*/ }
   post 'session' => 'session#create'
